2c61b23c1970d1799d085a12c4b1cf60fd4e535a
DzianisH <gavrilovetsden@gmail.com>
2020-04-20 15:34:16 +0300

    init

---
+++ src/test/java/org/demo/CaveatEmptor/CaveatEmptorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.CaveatEmptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaveatEmptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
